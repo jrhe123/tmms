96ea96ff80089b86370e82d0e97b77782f15bfd8
jrhe123 <keith_he@outlook.com>
2024-10-13 16:49:33 -0400

    add more func in string utils

--- src/base/StringUtils.cpp
+++ src/base/StringUtils.cpp
@@ -2,7 +2,7 @@
 
 using namespace tmms::base;
 
-bool StartsWith(const string &s, const string &sub)
+bool StringUtils::StartsWith(const string &s, const string &sub)
 {
   if (sub.empty())
   {
@@ -21,7 +21,7 @@ bool StartsWith(const string &s, const string &sub)
   return s.compare(0, sublen, sub) == 0;
 }
 
-bool EndsWith(const string &s, const string &sub)
+bool StringUtils::EndsWith(const string &s, const string &sub)
 {
   if (sub.empty())
   {
@@ -38,4 +38,92 @@ bool EndsWith(const string &s, const string &sub)
     return false;
   }
   return s.compare(len - sublen, sublen, sub) == 0;
+}
+
+string StringUtils::FilePath(const std::string &path)
+{
+  auto pos = path.find_last_of('/\\');
+  if (pos != std::string::npos)
+  {
+    // found
+    return path.substr(0, pos);
+  }
+  else
+  {
+    return "./";
+  }
+}
+
+string StringUtils::FileNameExt(const std::string &path)
+{
+  auto pos = path.find_last_of('/\\');
+  if (pos != std::string::npos)
+  {
+    // found
+    if (pos + 1 < path.size())
+    {
+      return path.substr(pos + 1);
+    }
+  }
+
+  return path;
+}
+
+string StringUtils::FileName(const std::string &path)
+{
+  string file_name = FileNameExt(path);
+  auto pos = file_name.find_last_of('.');
+  if (pos != std::string::npos)
+  {
+    // found
+    if (pos != 0)
+    {
+      return file_name.substr(0, pos);
+    }
+  }
+  return file_name;
+}
+
+string StringUtils::Extension(const std::string &path)
+{
+  string file_name = FileNameExt(path);
+  auto pos = file_name.find_last_of('.');
+  if (pos != std::string::npos)
+  {
+    // found
+    if (pos != 0 && pos + 1 < file_name.size())
+    {
+      return file_name.substr(pos + 1);
+    }
+  }
+  // empty string
+  return string();
+}
+
+vector<string> StringUtils::SplitString(const string &s, const string &delimiter)
+{
+  if (delimiter.empty())
+  {
+    return vector<string>{};
+  }
+
+  vector<string> result;
+  size_t last = 0;
+  size_t next = 0;
+
+  while ((next = s.find(delimiter, last)) != std::string::npos)
+  {
+    if (next > last)
+    {
+      result.emplace_back(
+          s.substr(last, next - last));
+    }
+    last = next + delimiter.size();
+  }
+  if (last < s.size())
+  {
+    result.emplace_back(s.substr(last));
+  }
+
+  return result;
 }
--- src/base/StringUtils.h
+++ src/base/StringUtils.h
@@ -1,17 +1,26 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace tmms
 {
   namespace base
   {
     using std::string;
+    using std::vector;
+
     class StringUtils
     {
     public:
       static bool StartsWith(const string &s, const string &sub);
       static bool EndsWith(const string &s, const string &sub);
+
+      static string FilePath(const std::string &path);
+      static string FileNameExt(const std::string &path);
+      static string FileName(const std::string &path);
+      static string Extension(const std::string &path);
+      static vector<string> SplitString(const string &s, const string &delimiter);
     };
   }
 }
